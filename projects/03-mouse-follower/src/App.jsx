03e223092f121b168c12a48aa385a785fd7b1437
ralfxdev <lopezalexander1019@gmail.com>
2023-04-20 22:12:46 -0600

    03-mouse-follower

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled]);
+
+  return (
+    <>
+      <main>
+        <div style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }} />
+        <h3>Mouse Follower</h3>
+        <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? "Desactivar" : "Activar"}
+        </button>
+      </main>
+    </>
+  );
+}
+
+export default App;
